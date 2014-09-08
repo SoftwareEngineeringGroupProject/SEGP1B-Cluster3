a0ed7d9308fbd85d47040180ebdd760d7a61568d
KhanhHB <a1615322@student.adelaide.edu.au>
2014-09-09 01:14:26 +0930

    Adding dashboard controller, gem jquery-turbo.., dashboard/view.html.erb. Editing application.html.erb and applicatoin.js, routes. Moving images and stylesheet with scripts to assets folder

---
+++ App/app/controllers/dashboards_controller.rb
@@ -0,0 +1,51 @@
+class DashboardsController < ApplicationController
+
+  def view
+    # View projects based on project state
+    @state = params[:state]
+
+
+
+
+    @projects = Project.all.select{|project| project.status == @state};
+   
+  end
+
+  def change_state
+    # Possible status
+    status = ["accepted", "pending", "new", "rejected"]
+
+    # If user have selected a project to apply an action
+    if (params[:project_id] != nil)
+
+      @project = Project.find(params[:project_id])
+
+
+      if ( status.include? params[:commit] )
+      # This is for updating status
+        @project.update_attributes({:status => params[:commit]})    
+      else 
+      # Specific actions 
+        if ( params[:commit]=="remove" )
+          delete
+        end
+      end  
+
+    end  
+
+    # Anyway, just render the same page
+    @projects = Project.all.select{|project| project.status == params[:state]};
+    @state = params[:state]
+    render :view
+
+  end
+
+  private
+
+    def delete
+     Project.destroy(params[:project_id]);
+    end
+
+
+
+end
--- App/app/controllers/submissions_controller.rb
+++ App/app/controllers/submissions_controller.rb
@@ -1,43 +1,55 @@
 class SubmissionsController < ApplicationController
-	
+  
   def index 
+    
   end
 
   def new
+    # Create a submission
     @project = Project.new
-	@company = Company.new
-	@attached = Attached.new
+    @company = Company.new
+    @attached = Attached.new
   end
 
   def create
-	@company = Company.new(company_params)
-	@project = @company.projects.build(project_params)
-	@attached =@project.build_attached(attached_params)
 
-	if @project.save && @company.save && @attached.save
-	  redirect_to submission_path(@project)
-	else
-	  render :new
-	end	
+    # Create association between new objects
+    @company = Company.new(company_params)
+    @project = @company.projects.build(project_params)
+    @attached =@project.build_attached(attached_params)
+
+    # Set state for newly submitted project 
+    @project.status = "new"
+
+    # If submission is done successfully, redirect to show page
+    if @project.save && @company.save && @attached.save
+      redirect_to submission_path(@project)
+    else
+    # Else, render the submission page  
+      render :new
+    end 
 
   end
 
   def show
-  	@project = Project.find(params[:id]);
-  	@company = @project.company
-  	@attached = @project.attached
+    # Show what user has submited
+    @project = Project.find(params[:id]);
+    @company = @project.company
+    @attached = @project.attached
   end
 
+
+  # Required due to strong parameter mechanisms
   private
     def project_params
       params.require(:project).permit(:title, :body)
-    end	
+    end 
 
     def company_params
       params.require(:company).permit(:name, :address, :phone, :email, :website)
-    end	
+    end 
 
     def attached_params
       params.require(:attached).permit(:attached, :project_id)
-    end 	
+    end   
 end
