58a3cf6db54d093a222351960412064ab8194192
KhanhHB <a1615322@student.adelaide.edu.au>
2014-10-07 03:36:14 +1030

    Admin can send an email with an attachment now

--- App/app/mailers/user_mailer.rb
+++ App/app/mailers/user_mailer.rb
@@ -11,10 +11,14 @@ class UserMailer < ActionMailer::Base
     mail(:to => "#{user.lname} <#{user.email}>", :subject => 'Password Reset')
   end
 
-  def send_a_message(message, receipient, subject)
+  def send_a_message(message, receipient, subject, file)
     @message = message
     @receipient = receipient
     @subject = subject
+
+    # Assign attachment from user's attached file
+    attachments[file.original_filename] = file.read unless file == nil
+
     mail(:to => "#{receipient.name} <#{receipient.email}>", :subject => subject)
   end
 
