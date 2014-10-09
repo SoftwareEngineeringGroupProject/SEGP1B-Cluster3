a0557196e82df9ab625cd88f05c41e97e8e068cf
kamalarieff <kamal.arieff@gmail.com>
2014-10-10 00:30:14 +1030

    it is a bit broken now need to find out how to modify model which has a has_one relationship student_project has a has_one relationship with group need to use nested forms

--- App/db/schema.rb
+++ App/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141008014747) do
+ActiveRecord::Schema.define(version: 20141009134543) do
 
   create_table "attacheds", force: true do |t|
     t.integer  "project_id"
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 20141008014747) do
 
   create_table "groups", force: true do |t|
     t.string   "name"
-    t.string   "groupID"
     t.integer  "numOfMembers"
     t.integer  "StudentProject_id"
     t.datetime "created_at"
