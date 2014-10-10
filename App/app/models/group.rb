e3634db8bbab3903aa3298ec326567f2dad5346e
kamalarieff <kamal.arieff@gmail.com>
2014-10-10 13:16:36 +1030

    nested forms is now working, can now submit past projects which linked to group model

--- App/app/models/group.rb
+++ App/app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ActiveRecord::Base
 	belongs_to :StudentProject
 	has_many :students
-	validates_presence_of :name, :groupID, :numOfMembers
+	validates_presence_of :name, :numOfMembers
 end
