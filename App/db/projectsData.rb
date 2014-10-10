8c7d37c1e77aff1465db766654b71a61025ba0ab
Andrew <andrewnguyen.x@gmail.com>
2014-10-10 12:56:57 +1030

    added past projects data

---
+++ App/db/projectsData.rb
@@ -0,0 +1,81 @@
+# #PROJECT 1
+# 	title:
+# 	QB50 SUSat CubeSat Nanosatellite Project
+
+# 	client:
+# 	QB50, The University of Adelaide
+
+# 	overview:
+# 	The QB50 project involves launching a network of 61 independently developed miniature satellites into the lower thermosphere to carry out research on the sections of the atmosphere rarely studied. The project is headed by the Belgian Von Karman Institute for Fluid Dynamics. SuSat is one of those satellites, which is being developed by the University of Adelaide and the University of South Australia.
+
+# 	members:
+# 	Scott Lamacraft, BEng(Software), a1607969@student.adelaide.edu.au
+# 	Sergei Fadeev, BEng(Software), a1608693@student.adelaide.edu.au
+# 	Samuel Wong, BEng(Software), a1607031@student.adelaide.edu.au
+
+# #PROJECT 2
+# 	title:
+# 	A mobile e-learning platform for Medical Students
+
+# 	client:
+# 	The Adelaide Simulation Centre
+
+# 	overview:
+# 	The motivation behind Respond is to ensure that medical students have the skills required to deal with emergency situations that will arise during their career. The Adelaide Simulation centre has students perform these kinds of scenarios but Respond is being developed to allow the students some time outside of the Centre to improve their skills on the iOS platform.
+
+# 	Scenario Engine is a web-based system that helps users to build the medical scenarios for the Respond. Scenario Engine provide a platform for medical staff to build the scenario by filling in the medical data into a set of forms, which will be used to generate the scenario package and store in the system for later use.
+
+# 	members:
+# 	Beri Ramsey, BEng(Software), a1607969@student.adelaide.edu.au***
+# 	Xi Chen, BEng(Software), a1634403@student.adelaide.edu.au
+# 	Xianhe Li, BEng(Software), a1614807@student.adelaide.edu.au
+
+# #PROJECT 3
+# 	title:
+# 	Radiometri Correction of Image Sets Using Genetic Programming
+
+# 	client:
+# 	Maptek
+
+# 	overview:
+# 	The project aims to normalise the radiometric characteristics of sets of interrelated photos. Application of this project will remove the impact of changing lighting conditions during extended image capture.
+
+# 	The potos are produced for Maptek by an overhead drone. After radiometric smoothing and other quality improving procedures, the photos are used to create a 3D model of the space, as well as large composite image. These products are extremely accurate and have a cariety of uses.
+
+# 	members:
+# 	Michael Stimson, BEng(Software), a1608486@student.adelaide.edu.au
+# 	William Reid, BEng(Software), a1215621@student.adelaide.edu.au
+# 	Pengcheng Zhou, BEng(Software), a1218763@student.adelaide.edu.au
+
+# #PROJECT 4
+# 	title:
+# 	Deconvolution of Aerial Photography
+
+# 	client:
+# 	Maptek
+
+# 	overview:
+# 	Photography taken from a moving aerial vehicle will inherently have minor amounts of blurring due to motion. To use the photographs in mapping, feature-matching is employed to calculate how they overlap. Even a small blue amount can significantly affect the results of these calculations.
+
+# 	members:
+# 	Ken Ear, BEng(Software), ken.ear@student.adelaide.edu.au
+# 	Dominik Jarco, BEng(Software), dominik.jarco@student.adelaide.edu.au
+# 	Josiah Saunders, BEng(Software), josiah.saunders@student.adelaide.edu.au
+
+# #PROJECT 5
+# 	title:
+# 	Breadcrumbs
+
+# 	client:
+# 	Airborne Research Australia
+
+# 	overview:
+# 	ARA uses light aircraft to perform aerial mappings. They do this by flying in parallel lines across the area. During these flights it's possible for spots to be missed, and this will not be known until the data is processed post-flight.
+
+# 	ARA needs a tool to indicate spots that have been missed while they are in-flight.
+
+# 	members:
+# 	Craig Lawrence, BEng(Software), craig.lawrence@student.adelaide.edu.au
+# 	Aidan Snoswell, BEng(Software), aidan.snoswell@student.adelaide.edu.au
+# 	Alec Bellati, BEng(Software), alec.bellati@student.adelaide.edu.au
+
