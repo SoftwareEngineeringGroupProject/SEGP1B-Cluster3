7f376c110e26edc1e55c262305ca131378700be8
Sam Reid <a1607876>
2014-10-11 18:03:58 +1030

    fixing seed db issues

--- App/config/initializers/seeddb.rb
+++
@@ -1,3 +0,0 @@
-#create initial admin
-@initialadmin = User.new({:username => "administrator", :email => "admin@admin.com", :password => "cluster3", :password_confirmation => "cluster3", :fname => "Default", :lname => "Admin", :companyname => "Cluster 3", :address => "NA", :phone => "NA", :website => "NA", :acctype => "coordinator"})
-@initialadmin.save
