be56f9bb172920c9c7affd9ae865cdc9ed18b908
DarkBlue-Small <wudylk@hotmail.com>
2014-10-21 17:50:10 +1030

    add another test type

---
+++ App/spec/features/password_tests_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe "PasswordResets" do
+  it "login user with invalid account" do
+    user = FactoryGirl.create(:user)
+    visit login_path
+    fill_in "username_or_email", :with => user.username
+    fill_in "login_password", :with => user.password
+    click_button "LOG IN"
+    page.should have_content("Invalid Username or Password")
+  end
+
+  it "login user with valid account" do
+    
+  end
+end
+
