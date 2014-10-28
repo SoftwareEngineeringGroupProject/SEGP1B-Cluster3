ad2e11b1cc5450f39ab6437db1066da4c9a8edab
DarkBlue-Small <wudylk@hotmail.com>
2014-10-29 02:54:28 +1030

    add test cases for industry project submission

--- App/spec/features/submitProject_tests_spec.rb
+++ App/spec/features/submitProject_tests_spec.rb
@@ -2,5 +2,20 @@ require 'spec_helper'
 
 describe "submit a new project" do
 
+	it "title cannot be blank" do
+		visit submit_project_path
+		fill_in "title", :with => ""
+		fill_in "body", :with => "This is a sample description"
+		click_button "SUBMIT"
+		page.should have_content("Title can't be blank")
+	end
+
+	it "description cannot be blank" do
+		visit submit_project_path
+		fill_in "title", :with => "This is a sample title"
+		fill_in "body", :with => ""
+		click_button "SUBMIT"
+		page.should have_content("Body can't be blank")
+	end
 
 end
