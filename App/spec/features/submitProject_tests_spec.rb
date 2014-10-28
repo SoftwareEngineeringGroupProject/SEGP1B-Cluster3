b562ade0f27bf5c7d432159843a7d08e9d3f3f80
DarkBlue-Small <wudylk@hotmail.com>
2014-10-29 02:57:29 +1030

    add another critical test cases for industry project submission

--- App/spec/features/submitProject_tests_spec.rb
+++ App/spec/features/submitProject_tests_spec.rb
@@ -18,4 +18,12 @@ describe "submit a new project" do
 		page.should have_content("Body can't be blank")
 	end
 
+	it "a long description should be valid" do
+		visit submit_project_path
+		fill_in "title", :with => "This is a sample title"
+		fill_in "body", :with => "This will be a long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long description"
+		click_button "SUBMIT"
+		page.should have_content("Thank You Very Much")
+	end
+
 end
