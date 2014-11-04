aa65e82dc02f8d136a46148ea3741595c29ecac8
thatscool <a1627698@student.adelaide.edu.au>
2014-11-04 22:14:41 +1030

    more rspec test cases

--- App/spec/requests/static_pages_spec.rb
+++ App/spec/requests/static_pages_spec.rb
@@ -4,29 +4,57 @@ describe "Static pages" do
 
 	subject { page }
 
+
+ # TESTING PAGE PATHS AND SIMPLE CONTENT
 	describe "Home page" do
-		before { visit  }
+		before { visit '/' }
+    it { should have_content('Home') }
+  end
 
+  describe "Login page" do
+    before { visit '/login' }
+    it { should have_content('Login') }
+  end
 
+  describe "Register page" do
+    before { visit '/signup' }
+    it { should have_content('Industry Partner Signup') }
+  end
 
+  describe "Project Portfolio page" do
+    before { visit '/projects' }
+    it { should have_content('Project Portfolio') }
+  end
+
+  describe "Information page" do
+    before { visit '/projects/info' }
+    it { should have_content('Information') }
+  end
+
+  describe "Contact page" do
+    before { visit '/contacts' }
+    it { should have_content('Contacts') }
+  end
+
+  describe "Bad URL page" do
+    before { visit '/adaiediuw123u235k3hrs/321edsdr43' }
+    it { should have_content('404') }
+  end
+
+  #TESTING LINKS
+  it "should have the right links on the layout" do
+    visit '/'
+    click_link "Home"
+    expect(page).to have_content('Home')
+    click_link "Login"
+    expect(page).to have_content('Login')
+    click_link "Register"
+    expect(page).to have_content('Industry Partner Signup')
+    click_link "Project Gallery"
+    expect(page).to have_content('Project Portfolio')
+    click_link "Information"
+    expect(page).to have_content('Information')
+    click_link "Contact"
+    expect(page).to have_content('Contacts')
+  end
 end
-
-
-
-describe "Static pages" do
-
-  subject { page }
-
-  shared_examples_for "all static pages" do
-    it { should have_selector('h1', text: heading) }
-    it { should have_title(full_title(page_title)) }
-  end
-
-  describe "Home page" do
-    before { visit root_path }
-    let(:heading)    { 'Sample App' }
-    let(:page_title) { '' }
-
-    it_should_behave_like "all static pages"
-    it { should_not have_title('| Home') }
-  end
